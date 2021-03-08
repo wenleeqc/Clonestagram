7c716cb2cc441389d5a242874f60a4adf4df0af9
wenleeqc <wenleeqc@gmail.com>
2021-03-07 22:57:26 -0500

    instagram clone part 1 update

--- app/src/main/java/com/example/clonestagram/LoginActivity.java
+++ app/src/main/java/com/example/clonestagram/LoginActivity.java
@@ -26,6 +26,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if(ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
@@ -59,5 +63,6 @@ public class LoginActivity extends AppCompatActivity {
     private void goMainActivity() {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/clonestagram/ParseApplication.java
+++ app/src/main/java/com/example/clonestagram/ParseApplication.java
@@ -3,11 +3,15 @@ package com.example.clonestagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("rYf1IKH1OOZM0o621saGczOFtGC2wAwjLaBCyJ5C")
